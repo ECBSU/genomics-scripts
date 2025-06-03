05764bd63689ee82f2f3cdbd09d97b1c4a5e0d16
Yong Heng Phua <107157547+pyongheng@users.noreply.github.com>
2025-06-04 08:30:49 +0900

    Update blobtools_diamond.sh

--- genome_binning/blobtools1/blobtools_diamond.sh
+++ genome_binning/blobtools1/blobtools_diamond.sh
@@ -3,7 +3,7 @@
 sed 's/|/\t/g' "/flash/HusnikU/Phua_2/Sino_genome/SPAdes_all_seq/scaffolds_v_sprot.diablastx" | awk '{$2=$4=""; print $0}' OFS="\t" > scaffolds_v_sprot.diablastx
 export PYTHONPATH="/apps/unit/HusnikU/pysam/lib64/python3.6/site-packages/:$PYTHONPATH"
 /apps/unit/HusnikU/blobtools/blobtools taxify -f scaffolds_v_sprot.diablastx -m "/flash/HusnikU/databases/reference_proteomes.taxid_map" -s 0 -t 2
-/apps/unit/HusnikU/blobtools/blobtools create -i scaffolds.fasta -y spades -t scaffolds_v_sprot.diablastx2.taxified.out --db /home/y/yong-phua/blobtaxid/nodesDB.txt 
+/apps/unit/HusnikU/blobtools/blobtools create -i scaffolds.fasta -y spades -t scaffolds_v_sprot.diablastx2.taxified.out --db /bucket/.deigo/HusnikU/Databases/blobtaxid/nodesDB.txt 
 /apps/unit/HusnikU/blobtools/blobtools plot -i blobDB.json
 /apps/unit/HusnikU/blobtools/blobtools plot -i blobDB.json -r order
 /apps/unit/HusnikU/blobtools/blobtools view -i blobDB.json -r phylum -r order -r family 
